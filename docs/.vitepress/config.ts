92be6a5c4025f526cd0fd860cf5deac551f4a41f
skirtle <65301168+skirtles-code@users.noreply.github.com>
2023-06-26 04:47:12 +0100

    Reorder questions

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,27 +21,39 @@ export default defineConfig({
             link: '/faq/learning-vue'
           },
           {
-            text: `How do I check which version I'm using?`,
-            link: '/faq/checking-versions'
+            text: 'Can you show me a large, open-source Vue project that I can study?',
+            link: '/faq/large-example-applications'
+          },
+          {
+            text: 'Which Vue component library should I use?',
+            link: '/faq/component-library'
           },
           {
             text: 'What folder structure should I use for my Vue project?',
             link: '/faq/folder-structure'
           },
           {
+            text: 'Can I use Nuxt features without using Nuxt?',
+            link: '/faq/nuxt-features'
+          },
+          {
+            text: 'Do Vue 2 components work with Vue 3?',
+            link: '/faq/vue-2-components-in-vue-3'
+          },
+          {
             text: 'I need users to stay logged in if they refresh the page. Should I use cookies or local storage?',
             link: '/faq/cookies-local-storage'
           },
           {
-            text: 'How should my components communicate?',
-            link: '/faq/component-communication'
+            text: `How do I check which version I'm using?`,
+            link: '/faq/checking-versions'
           },
           {
-            text: 'Why do my dynamic images not work?',
-            link: '/faq/dynamic-images'
+            text: 'How do I run an old project?',
+            link: '/faq/running-old-projects'
           },
           {
-            text: 'When does my page fail to load when I refresh in production?',
+            text: 'Why does my page fail to load when I refresh in production?',
             link: '/faq/production-page-refresh'
           },
           {
@@ -49,60 +61,48 @@ export default defineConfig({
             link: '/faq/github-pages'
           },
           {
-            text: 'Which Vue component library should I use?',
-            link: '/faq/component-library'
+            text: `In my SPA, how to add og tags and why doesn't Twitter/Facebook see them when I add them dynamically?`,
+            link: '/faq/og-tags'
+          },
+          {
+            text: 'How should my components communicate?',
+            link: '/faq/component-communication'
           },
           {
             text: 'How can I pass slots through to a child component?',
             link: '/faq/forwarding-slots'
           },
           {
-            text: 'Can I use Nuxt features without using Nuxt?',
-            link: '/faq/nuxt-features'
-          },
-          {
-            text: 'Can you show me a large, open-source Vue project that I can study?',
-            link: '/faq/large-example-applications'
-          },
-          {
-            text: 'When to use reactive() and ref()?',
-            link: '/faq/reactive-ref'
-          },
-          {
             text: `How can I make Vue 'wait' for the data before rendering?`,
             link: '/faq/delaying-rendering'
-          },
-          {
-            text: `Why does my logging show an empty/missing value after I've loaded the data?`,
-            link: '/faq/logging-after-loading'
           },
           {
             text: "Why isn't v-html rendering my components?",
             link: '/faq/components-in-v-html'
           },
           {
-            text: 'Why are the new value and old value the same in my watcher?',
-            link: '/faq/deep-watcher-values'
+            text: `Why does my logging show an empty/missing value after I've loaded the data?`,
+            link: '/faq/logging-after-loading'
           },
           {
             text: 'Why can I see my data in console logging, but if I try to access it I get undefined or an error?',
             link: '/faq/logging-is-live'
           },
           {
-            text: `In my SPA, how to add og tags and why doesn't Twitter/Facebook see them when I add them dynamically?`,
-            link: '/faq/og-tags'
+            text: 'When to use reactive() and ref()?',
+            link: '/faq/reactive-ref'
+          },
+          {
+            text: 'Why are the new value and old value the same in my watcher?',
+            link: '/faq/deep-watcher-values'
+          },
+          {
+            text: 'Why do my dynamic images not work?',
+            link: '/faq/dynamic-images'
           },
           {
             text: 'Why are my dynamic Tailwind classes not working?',
             link: '/faq/missing-tailwind-classes'
-          },
-          {
-            text: 'Do Vue 2 components work with Vue 3?',
-            link: '/faq/vue-2-components-in-vue-3'
-          },
-          {
-            text: 'How do I run an old project?',
-            link: '/faq/running-old-projects'
           }
         ]
       }
--- docs/faq/index.md
+++ docs/faq/index.md
@@ -8,25 +8,67 @@ It is not a substitute for the official Vue documentation, available at <https:/
 We've only just started work on this FAQ and most of the answers will be incomplete. Some questions don't really have an answer yet, they're just placeholders.
 :::
 
+---
+
+<!-- Learning -->
+
 - [How should I learn Vue?](learning-vue)
-- [How do I check which version I'm using?](checking-versions)
-- [What folder structure should I use for my Vue project?](folder-structure)
-- [I need users to stay logged in if they refresh the page. Should I use cookies or local storage?](cookies-local-storage)
-- [How should my components communicate?](component-communication)
-- [Why do my dynamic images not work?](dynamic-images)
-- [When does my page fail to load when I refresh in production?](production-page-refresh)
-- [How do I deploy to GitHub Pages?](github-pages)
+- [Can you show me a large, open-source Vue project that I can study?](large-example-applications)
+
+---
+
+<!-- New project decisions -->
+
 - [Which Vue component library should I use?](component-library)
-- [How can I pass slots through to a child component?](forwarding-slots)
+- [What folder structure should I use for my Vue project?](folder-structure)
 - [Can I use Nuxt features without using Nuxt?](nuxt-features)
-- [Can you show me a large, open-source Vue project that I can study?](large-example-applications.html)
-- [When to use `reactive()` and `ref()`?](reactive-ref)
-- [How can I make Vue 'wait' for the data before rendering?](delaying-rendering)
-- [Why does my logging show an empty/missing value after I've loaded the data?](logging-after-loading)
-- [Why isn't `v-html` rendering my components?](components-in-v-html)
-- [Why are the new value and old value the same in my watcher?](deep-watcher-values)
-- [Why can I see my data in console logging, but if I try to access it in my code I get `undefined` or an error?](logging-is-live)
-- [In my SPA, how to add og tags and why doesn't Twitter/Facebook see them when I add them dynamically?](og-tags)
-- [Why are my dynamic Tailwind classes not working?](missing-tailwind-classes)
 - [Do Vue 2 components work with Vue 3?](vue-2-components-in-vue-3)
+- [I need users to stay logged in if they refresh the page. Should I use cookies or local storage?](cookies-local-storage)
+
+---
+
+<!-- Working with npm -->
+
+- [How do I check which version I'm using?](checking-versions)
 - [How do I run an old project?](running-old-projects)
+
+---
+
+<!-- Deployment -->
+
+- [Why does my page fail to load when I refresh in production?](production-page-refresh)
+- [How do I deploy to GitHub Pages?](github-pages)
+- [In my SPA, how to add og tags and why doesn't Twitter/Facebook see them when I add them dynamically?](og-tags)
+
+---
+
+<!-- Vue code patterns -->
+
+- [How should my components communicate?](component-communication)
+- [How can I pass all slots through to a child component?](forwarding-slots)
+- [How can I make Vue 'wait' for the data before rendering?](delaying-rendering)
+- [Why isn't `v-html` rendering my components?](components-in-v-html)
+- Can I create a local variable in my template? <!-- scoped slot or v-for trick -->
+- Why does selecting one item select them all?
+- Why are my template refs not working? <!-- `v-for="i in foo" :ref="`xxx${i}`"` and `ref.value.foo()` failing due to `v-for`. -->
+
+---
+
+<!-- Debugging -->
+
+- [Why does my logging show an empty/missing value after I've loaded the data?](logging-after-loading)
+- [Why can I see my data in console logging, but if I try to access it in my code I get `undefined` or an error?](logging-is-live)
+
+---
+
+<!-- Common Vue misunderstandings -->
+
+- [When to use `reactive()` and `ref()`?](reactive-ref)
+- [Why are the new value and old value the same in my watcher?](deep-watcher-values)
+
+---
+
+<!-- Common tooling problems -->
+
+- [Why do my dynamic images not work?](dynamic-images)
+- [Why are my dynamic Tailwind classes not working?](missing-tailwind-classes)
